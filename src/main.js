6faec34337a51fee53bdceb22e20d074581fe735
sd3428783 <419600150@qq.com>
2020-05-20 15:16:34 +0800

    修改商品列表页

--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 import TreeTable from 'vue-table-with-tree-grid'
 import axios from 'axios'
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+//导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 //配置请求得根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
@@ -19,6 +25,20 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table',TreeTable)
+//将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat',function(originVal) {
+  const dt = new Date(originVal)
+
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2,'0')
+  const d = (dt.getDate() + '').padStart(2,'0')
+  const hh = (dt.getHours() + '').padStart(2,'0')
+  const mm = (dt.getMinutes() + '').padStart(2,'0')
+  const ss = (dt.getSeconds() + '').padStart(2,'0')
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import { Button, Form, FormItem, Input } from 'element-ui'
-import { Tabs,TabPane,Alert,Cascader, Tree,Tag,MessageBox,Dialog,Pagination,Tooltip,Switch,Table,TableColumn,Row,Col,Select,Option,Card,BreadcrumbItem,Breadcrumb, Message, Container, Header,Aside,Main,Menu,Submenu,MenuItemGroup,MenuItem } from 'element-ui'
+import { Upload,CheckboxGroup,Checkbox,Steps,Step,Tabs,TabPane,Alert,Cascader, Tree,Tag,MessageBox,Dialog,Pagination,Tooltip,Switch,Table,TableColumn,Row,Col,Select,Option,Card,BreadcrumbItem,Breadcrumb, Message, Container, Header,Aside,Main,Menu,Submenu,MenuItemGroup,MenuItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -33,5 +33,10 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
